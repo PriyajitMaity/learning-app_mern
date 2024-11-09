1d1e8b724148d87d6e727419de4dc8a21d38c5cc
Priyojeet Maity <priyajit0270@gmail.com>
2024-11-09 11:57:59 +0530

    fix my-courses double print error

--- server/controllers/Students/studentCoursesController.js
+++ server/controllers/Students/studentCoursesController.js
@@ -7,18 +7,14 @@ const getCourseByStudentId = async (req, res) => {
     const studentBoughtCourses = await StudentCourse.findOne({
       userId: studentId,
     });
-
-    if (!studentBoughtCourses) {
-      return res.status(404).json({
-        success: false,
-        message: "Student not found",
-      });
-    } else {
-      res.status(200).json({
+    
+    if(studentBoughtCourses){
+       res.status(200).json({
         success: true,
         data: studentBoughtCourses.courses,
       });
-    }
+    } 
+   
   } catch (error) {
     console.log(error);
     res.status(500).json({
